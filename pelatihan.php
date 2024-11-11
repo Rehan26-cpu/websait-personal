7058c13d54eab8a2f15e56f6603426ff5cc3c5ec
Rehan_ <mrehanpaujan26@gmail.com>
2024-11-11 14:55:56 +0700

    Add files via upload

---
+++ pelatihan.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> pendidikan </title>
+    </head>
+    <body>
+        <table width="30%" border="3" cellpadding='0'>
+            <tr> <th colspan="2" bgcolor="#00FFFF" align="center" height="20"> pelatihan </th></tr>
+            <tr height="50" bgcolor="#00FF00"> <th> Tahun </th> <th> Asal Sekolah </th></tr>
+            <tr> <th> 2019 </th><th> seminar nasional</th></tr>
+            <tr> <th> 2021</th><th> dewan kerja ranting  </th></tr>
+            <tr> <th> 2023</th><th> kaderisasi </th></tr>
+            <tr> <th> 2024 </th><th> mandiri </th></tr>
+            <tr> <th> 2023-Sekarang </th><th> mahasiswa</th></tr>
+
+        </table>
+    </body>
+</html>
